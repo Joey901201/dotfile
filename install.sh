83733b60f680f5db402e6f512a639613e97651b5
Joey901201 <joey901201@gmail.com>
2025-02-27 18:16:28 -0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+filter=""
+dry_run="0"
+
+while [[ $# -gt 0 ]]; do
+    if [[ "$1" == "--dry" ]]; then
+        dry_run="1"
+    else
+        filter="$1"
+    fi
+    shift
+done
+
+log() {
+    if [[ "$dry_run" == "1" ]]; then
+        echo "[DRY_RUN]: $@"
+    else
+        echo "$@"
+    fi
+}
+
+execute() {
+    log "execute $@"
+    if [[ "$dry_run" == "1" ]]; then
+        return
+    fi
+    "$@"
+}
+
+log "$script_dir -- $filter"
+cd $script_dir
+
+scripts=$(find ./installs -maxdepth 1 -mindepth 1 -executable -type f)
+
+for script in $scripts; do
+
+done
